b4fed56eed4eca778f4a267759d1dad95324fd7a
是你的阿龙丶 <1561445463@qq.com>
2020-08-12 00:24:28 +0800

    删除一些不使用的类

--- app/src/main/java/net/along/fragonflyfm/util/GetTime.java
+++ app/src/main/java/net/along/fragonflyfm/util/GetTime.java
@@ -1,15 +1,28 @@
 package net.along.fragonflyfm.util;
 
-
 import java.util.Calendar;
+
+/**
+ * 获取时间类
+ */
 
 public class GetTime {
     private static Calendar calendar = Calendar.getInstance();
 
+    /**
+     * 获取一周的时间
+     *
+     * @return
+     */
     public static int dayOFWeek() {
         return calendar.get(Calendar.DAY_OF_WEEK);
     }
 
+    /**
+     * 获取时分秒
+     *
+     * @return
+     */
     public static String getTime() {
         int hour = calendar.get(Calendar.HOUR_OF_DAY);
         int minute = calendar.get(Calendar.MINUTE);
@@ -17,6 +30,11 @@ public class GetTime {
         return "" + hour + minute + second;
     }
 
+    /**
+     * 获取年月日
+     *
+     * @return
+     */
     public static String getDate() {
         int year = calendar.get(Calendar.YEAR);
         int monthInt = 1 + calendar.get(Calendar.MONTH);
@@ -24,23 +42,5 @@ public class GetTime {
         int days = calendar.get(Calendar.DAY_OF_MONTH);
         String day = days > 9 ? days + "" : "0" + days;
         return "" + year + month + day;
-    }
-
-    public static String changeToPlayUrl(int channelId, String startTime, String endTime) {
-        startTime = startTime.replace(":", "");
-        endTime = endTime.replace(":", "");
-        String urlStr = "https://lcache.qingting.fm/cache/" + getDate() + "/"
-                + channelId + "/" + channelId + "_" + getDate() + "_" + startTime +
-                "_" + endTime + "_24_0.aac";
-        return urlStr;
-
-        //https://lhttp.qingting.fm/live/ "content_id =1259"/64k.mp3?app_id=web&ts=5f2d0afe&sign=72cd8cbc545f646c05bc74cedcdf4f1e
-        //https://lhttp.qingting.fm/live/1753/64k.mp3?app_id=web&ts=5f2d0b7f&sign=7a3f3667a476d8a3ed40c27da5cb3176
-        //https://lhttp.qingting.fm/live/1753/64k.mp3?app_id=web&ts=5f2d0bad&sign=ec1c3115ceb1ec7ab08905e7b24235d0
-    }
-
-    public static String Url() {
-        String url = "https://lhttp.qingting.fm/live/1753/64k.mp3?app_id=web&ts=5f2d0bad&sign=ec1c3115ceb1ec7ab08905e7b24235d0";
-        return url;
     }
 }
